14a5a475f1ac848b744cc766722c7ea8614e7fcb
Antonio <antonio@ecommercecandles.com>
2020-02-06 17:13:48 +0100

    1 New extension

---
+++ hybris/bin/custom/arecocmscomponents/src/org/areco/ecommerce/cms/components/service/ArecocmscomponentsService.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2019 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package org.areco.ecommerce.cms.components.service;
+
+public interface ArecocmscomponentsService
+{
+	String getHybrisLogoUrl(String logoCode);
+
+	void createLogo(String logoCode);
+}
